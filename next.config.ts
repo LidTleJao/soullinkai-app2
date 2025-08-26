689ecacafdc9d3d9e0040baf0203401c52e4368b
LidTleJao <praphunpongtke@gmail.com>
2025-08-26 18:00:55 +0700

    fix deploy firebase

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // output: "export", // เพิ่มบรรทัดนี้
+  images: { unoptimized: true },
+  output: "export", // เพิ่มบรรทัดนี้
 };
 
 export default nextConfig;
